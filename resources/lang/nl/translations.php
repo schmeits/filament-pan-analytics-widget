951ed7987ea72f3e3910fda75348eedf930eb407
Tally Schmeits <tally@schmeits.com>
2024-10-14 13:24:11 +0200

    Dutch translations

---
+++ resources/lang/nl/translations.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'widget' => [
+        'global' => [
+            'limit' => 'top :count resultaten',
+            'limit_show_all' => 'alle resultaten',
+        ],
+        'pan-analytics-table' => [
+            'heading' => 'Pan Analytics Tabel',
+            'headers' => [
+                'id' => '#',
+                'name' => 'Naam',
+                'impressions' => 'Vertoningen',
+                'hovers' => 'Hovers',
+                'clicks' => 'Klikken',
+            ],
+        ],
+    ],
+];
